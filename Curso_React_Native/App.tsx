167127a7a8e41b002a035985d79b24bcc75d1b25
Manuel Latorre <manuel.latorre@est.fi.uncoma.edu.ar>
2023-11-03 17:49:09 -0300

    Proyecto seteado ahora si

--- Curso_React_Native/App.tsx
+++ Curso_React_Native/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { View } from 'react-native';
-import { ProjectsRes } from "./Proyectos_res/ProjectsRes";
 
 function App(): JSX.Element {
   return (
     <View style={{ flex: 1 }}>
-      <ProjectsRes></ProjectsRes>
+      {/* TODO Aca van a poner los proyectos como si fueran componentes unicos */}
     </View>
   );
 }
